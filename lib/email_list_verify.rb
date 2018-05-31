78072ee5b9416c93b3fcba914cff77dbaa3e702f
data-plum <ivanka.feleschuk@gmail.com>
2018-05-31 13:49:49 +0300

    Fix BASE

--- lib/email_list_verify.rb
+++ lib/email_list_verify.rb
@@ -2,7 +2,7 @@ require "email_list_verify/version"
 
 require 'rest-client'
 class EmailListVerify
-  BASE = 'https://app.emaillistverify.com/api'
+  BASE = 'https://apps.emaillistverify.com/api'
   ONE_BY_ONE_PATH = BASE + "/verifEmail"
   UPLOAD_FILE_PATH = BASE + "/verifApiFile"
   FILE_INFO_PATH = BASE + "/getApiFileInfo"
